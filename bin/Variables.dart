7caa4bc2b89309567d1eeb33246c470416fb9270
Kateryna Nahorna <kitnahorna@gmail.com>
2023-08-18 18:26:00 +0300

    first commit

---
+++ bin/Variables.dart
@@ -0,0 +1,11 @@
+// не до кінця розумію в чому суть
+// Класс var успадковується від Object.
+//І var і в Object можна присвоїти будь-який тип даних.
+//Навіщо існує тип var тоді? Як заглянути в середину цих класів?
+var name = 'Bob';
+Object name1 = 'Bob';
+
+void main(List<String> arguments) {
+  print(name);
+  print(name1);
+}
