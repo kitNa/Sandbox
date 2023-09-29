12ce2414c0c2836fe7b32780725b8555e8909590
Kateryna Nahorna <kitnahorna@gmail.com>
2023-09-29 12:33:36 +0300

    completed the task of finding the optimal path through the maze

--- lib/src/exercises/005_patterns_switch_case/01_switch_case.dart
+++ lib/src/exercises/005_patterns_switch_case/01_switch_case.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:math';
 
+import 'package:sandbox/src/exercises/001_basics_variables/variables.dart';
+
 Future<void> main() async {
   // https://dart.dev/language/branches#switch-statements
   switchCase();
@@ -20,15 +22,104 @@ Future<void> switchCase() async {
   // Each move costs one point.
   // Each contestant's path is defined as a list of moves: "u" - up, "d" - down, "l" - left, "r" - right.
 
-  var mazeFile = File('lib/src/exercises/005_patterns_switch_case/the-great-maze.txt');
+  var mazeFile =
+      File('lib/src/exercises/005_patterns_switch_case/the-great-maze.txt');
   var mazeLines = await mazeFile.readAsLines();
-  var maze = mazeLines.map((line) => line.split('')).toList();
+  var maze = mazeLines
+      .where((line) => line.isNotEmpty)
+      .map((line) => line.split(''))
+      .toList();
+  //print(maze);
 
   // 1. Emulate several warriors, they need to go through the maze and reach the exit. Print their paths.
-  var rnd = Random();
-
+  const multiplier = 10;
+  const penalty = 5;
+  const moveCost = -1;
+  const path1 = 'rdddddrrrddddrrrrrrrddrrrddddrrrrrrrrrrd';
+  const path2 = 'rrrdddrruurrrrrrddrrrddddrrrrrrddddddrrrrddd';
+  const path3 = 'rrdddddddddddddddrrrrrrrrrrrrrrrrrrrrrrd';
+  var paths = [path1, path2, path3];
+  var firstPathCommands = path1.split('');
+  var secondPathCommands = path2.split('');
+  var thirdPathCommands = path3.split('');
 
   // 2. You're the judge of the competition. You have to choose the winner.
   // Find the mightiest warrior, who reaches the exit with the highest score. The contestant who doesn't reach the
   // exit is disqualified.
+  //Varianr1
+  print('\n***Var1***\n');
+  for (var path in paths) {
+    var bonus = 0;
+    var posRow = 0;
+    var posColumn = 0;
+    var pathCommands = path.split('');
+    for (var i = 0; i < pathCommands.length; i++) {
+      var command = pathCommands[i];
+      //print('Step i $i and maze posRow ${posRow + 1}, maze posColumn ${posColumn + 1}');
+      switch (command) {
+        case 'r':
+          posColumn++;
+        case 'l':
+          posColumn--;
+        case 'u':
+          posRow--;
+        case 'd':
+          posRow++;
+      }
+      if (int.tryParse(maze[posRow][posColumn]) != null) {
+        bonus += int.parse(maze[posRow][posColumn]) * multiplier;
+      } else if (maze[posRow][posColumn] == 'T') {
+        bonus -= penalty;
+      } else {
+        bonus -= moveCost;
+      }
+    }
+    var exitRowPos = maze.length - 1;
+    var exitColumnPos = maze[exitRowPos].length - 1;
+    if (posColumn == exitColumnPos && posRow == exitRowPos) {
+      print ('Вітаю! Ви вийшли із лабіринта. Ваш бонус: $bonus');
+    } else {
+      print('Ви заблукали!');
+    }
+  }
+//Varianr2
+  print('\n***Var2***\n');
+  for (var path in paths) {
+    var bonus = 0;
+    var posRow = 0;
+    var posColumn = 0;
+    var pathCommands = path.split('');
+    // var numberPattern = RegExp("[\\d]");
+     var numbers = ['1', '2', '3', '4', '5', '6'];
+    for (var i = 0; i < pathCommands.length; i++) {
+      var mazeElement = maze[posRow][posColumn].toString();
+      var command = pathCommands[i];
+      //print('Step i $i and maze posRow ${posRow + 1}, maze posColumn ${posColumn + 1}');
+      switch (command) {
+        case 'r':
+          posColumn++;
+        case 'l':
+          posColumn--;
+        case 'u':
+          posRow--;
+        case 'd':
+          posRow++;
+      }
+      switch (mazeElement) {
+        case '1' || '2' || '3' || '4' || '5' || '6':
+          bonus += int.parse(mazeElement) * multiplier;
+        case 'T':
+          bonus -= penalty;
+        default:
+          bonus -= moveCost;
+      }
+    }
+    var exitRowPos = maze.length - 1;
+    var exitColumnPos = maze[exitRowPos].length - 1;
+    if (posColumn == exitColumnPos && posRow == exitRowPos) {
+      print ('Вітаю! Ви вийшли із лабіринта. Ваш бонус: $bonus');
+    } else {
+      print('Ви заблукали!');
+    }
+  }
 }
