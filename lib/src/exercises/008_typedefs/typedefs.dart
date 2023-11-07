60d5384fb89ab8c816ca2300f6ae0a59c564d7e9
Kateryna Nahorna <kitnahorna@gmail.com>
2023-11-07 17:13:48 +0200

    added smoll changes to functions.dart

--- lib/src/exercises/008_typedefs/typedefs.dart
+++ lib/src/exercises/008_typedefs/typedefs.dart
@@ -1,7 +1,7 @@
 void main() {
   // https://dart.dev/language/typedefs
 
-  // 1. Define "IntList" typedef to be an alias for "List<int>". Create a list
+  // 1. Define "IntList" B to be an alias for "List<int>". Create a list
   // of numbers from 1 to 5 using "IntList" alias type.
 
   // 2. Define "Ages" typedef to be an alias for "Map<String, int>". Create a
--- lib/src/exercises/009_functions/functions.dart
+++ lib/src/exercises/009_functions/functions.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 void main() {
   // https://dart.dev/language/functions
 
@@ -10,4 +12,23 @@ void main() {
   // 3. Required named params
 
   // 4. Optional positional params
+
+  namedParams();
+}
+
+void namedParams() {
+  //1. Create a function with named parameters that will print information about
+  // the purchase of any product. Use the following parameters: date, seller, product, price, quantity
+//= DateTime(2023, 12, 03)
+  void printPurchaseInformation(DateTime date , String seller, String product,
+      double price, int quantity) {
+    print("дата: $date\n "
+        "продавець: $seller\n "
+        "товар: $product \n "
+        "ціна: $priceгрн\n "
+        "кількість: $quantity\n "
+        "вартість: ${price * quantity}");
+  }
+
+  printPurchaseInformation(date: DateTime.now() , seller: 'Vasyz', product, price, quantity)
 }
