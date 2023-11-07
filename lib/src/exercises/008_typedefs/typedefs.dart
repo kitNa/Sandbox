272e14de35fce50c8f9d1cfa3c02a0d535c11608
Kateryna Nahorna <kitnahorna@gmail.com>
2023-11-07 09:51:05 +0200

    added changes to typedef

--- lib/src/exercises/008_typedefs/typedefs.dart
+++ lib/src/exercises/008_typedefs/typedefs.dart
@@ -3,7 +3,11 @@ void main() {
 
   // 1. Define "IntList" typedef to be an alias for "List<int>". Create a list
   // of numbers from 1 to 5 using "IntList" alias type.
+  typedef IntList = List<int>;
+  IntList list = [1, 2, 3, 4, 5];
 
   // 2. Define "Ages" typedef to be an alias for "Map<String, int>". Create a
   // map of your family members and their ages using "Ages" alias type.
+  typedef Ages = Map<String, int>;
+  Ages myFamilyAge = [Katya: 32, Sasha: 42, Margosha: 5];
 }
