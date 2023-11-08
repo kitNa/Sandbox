81e7df3a377231f1299a259ac868c76774f343f9
Kateryna Nahorna <kitnahorna@gmail.com>
2023-11-08 09:34:43 +0200

    addad exercise to functions.dart

--- lib/src/exercises/009_functions/functions.dart
+++ lib/src/exercises/009_functions/functions.dart
@@ -17,11 +17,28 @@ void main() {
 }
 
 void namedParams() {
-  //1. Create a function with named parameters that will print information about
+  //1. Create a function with named parameters that will print a person's last
+  // name and first name. By default, fill in the data with your own name.
+  print('\n***namedParams1***\n');
+  void printFullName(
+      {String firstName = 'Katya',
+        String lastName = 'Nahorna'}) {
+    print("Ім'я: $firstName\n"
+        "Прізвище: $lastName");
+  }
+  printFullName(); //Ім'я: Katya, Прізвище: Nahorna
+  printFullName(lastName: 'Radchenko', firstName: 'Lena'); //Ім'я: Lena Прізвище: Radchenko
+  printFullName(lastName: 'Radchenko'); //Ім'я: Katya Прізвище: Radchenko
+
+  //2. Create a function with required  named parameters that will print information about
   // the purchase of any product. Use the following parameters: date, seller, product, price, quantity
-//= DateTime(2023, 12, 03)
-  void printPurchaseInformation(DateTime date , String seller, String product,
-      double price, int quantity) {
+  print('\n***namedParams2***\n');
+  void printPurchaseInformation(
+      {required DateTime date,
+      required String seller,
+      required String product,
+      required double price,
+      required int quantity}) {
     print("дата: $date\n "
         "продавець: $seller\n "
         "товар: $product \n "
@@ -29,6 +46,11 @@ void namedParams() {
         "кількість: $quantity\n "
         "вартість: ${price * quantity}");
   }
-
-  printPurchaseInformation(date: DateTime.now() , seller: 'Vasyz', product, price, quantity)
+  printPurchaseInformation(
+      date: DateTime.now(),
+      seller: 'Василь',
+      product: 'Ручка',
+      price: 5.5,
+      quantity: 20);
+  //printPurchaseInformation(); //The named parameters is required, but there's no corresponding argument
 }
