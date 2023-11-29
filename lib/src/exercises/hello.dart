88b8a54d1b28075e5260f3bdb004eeb7b7c5a7cb
Dmytro Buryak <dmytro.buryak@gmail.com>
2023-11-29 13:38:30 +0200

    Revert accidental changes in hello.dart

--- lib/src/exercises/hello.dart
+++ lib/src/exercises/hello.dart
@@ -1,15 +1,3 @@
 void main() {
-  var plane1 = Plane(name: 'Boeing 737');
-  var plane2 = Plane(name: 'Airbus A320');
-  var plane3 = Plane(name: 'Embraer 190', maxSpeed: 870);
-
-
-}
-
-class User {
-
-}
-
-class OurSystemException implements Exception {
-
+  print('hello from Katya and Dima. Resolved merge conflict');
 }
