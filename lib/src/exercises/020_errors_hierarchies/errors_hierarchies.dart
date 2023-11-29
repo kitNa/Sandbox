93bd88817c08457d90e9afa0b05f2453527b9be2
Dmytro Buryak <dmytro.buryak@gmail.com>
2023-11-29 13:33:52 +0200

    Add examples for exception hierarchies

--- lib/src/exercises/020_errors_hierarchies/errors_hierarchies.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  // https://dart.dev/language/error-handling
-
-  // TODO: add exercises for errors type hierarchies
-}
--- lib/src/exercises/hello.dart
+++ lib/src/exercises/hello.dart
@@ -1,3 +1,15 @@
 void main() {
-  print('hello from Katya and Dima. Resolved merge conflict');
+  var plane1 = Plane(name: 'Boeing 737');
+  var plane2 = Plane(name: 'Airbus A320');
+  var plane3 = Plane(name: 'Embraer 190', maxSpeed: 870);
+
+
+}
+
+class User {
+
+}
+
+class OurSystemException implements Exception {
+
 }
