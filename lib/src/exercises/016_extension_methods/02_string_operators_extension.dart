4957c420fce7ff8c873f3fde2c521815199ebaed
Kateryna Nahorna <kitnahorna@gmail.com>
2023-12-14 11:48:44 +0200

    some corrections was added

--- lib/src/exercises/016_extension_methods/02_string_operators_extension.dart
+++ lib/src/exercises/016_extension_methods/02_string_operators_extension.dart
@@ -1,8 +1,10 @@
 extension MyStringOperatorsExtension on String {
   String operator -(String letters) {
-    var newString = this.toLowerCase();
-    for(var i = 0; i < letters.length; i++) {
-      newString = newString.replaceAll(letters[i].toLowerCase(), '');
+    var newString = this;
+    for (var i = 0; i < letters.length; i++) {
+      newString = newString
+          .replaceAll(letters[i].toLowerCase(), '')
+          .replaceAll(letters[i].toUpperCase(), '');
     }
     return newString;
   }
